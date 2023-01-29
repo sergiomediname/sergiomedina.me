901e1619f24eeb258f247b1feffd4a2d5f5025af
Sergio Medina <sergiomedina.me@gmail.com>
2023-01-29 19:56:20 +0100

    FIX: Agregar attr href a enlace de linkein en componente social

--- components/Social.vue
+++ components/Social.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="flex items-center gap-6">
-        <!-- <a href="#"><iLinkedin class="i--lg" /></a
-        ><a href="https://www.behance.net/sergiomedinaweb"
+        <a href="https://linkedin.com/in/sergiomediname-me" target="_blank"
+            ><iLinkedin class="i--lg" /></a
+        ><a href="https://www.behance.net/sergiomedinaweb" target="_blank"
             ><iBehance class="i--lg" /></a
-        ><a href="https://github.com/sergiomediname"
+        ><a href="https://github.com/sergiomediname" target="_blank"
             ><iGithub class="i--lg"
-        /></a> -->
+        /></a>
     </div>
 </template>
 
 <script setup>
-// import iLinkedin from "@/components/icons/iLinkedin.vue";
-// import iBehance from "@/components/icons/iBehance.vue";
-// import iGithub from "@/components/icons/iGithub.vue";
+import iLinkedin from "@/components/icons/iLinkedin.vue";
+import iBehance from "@/components/icons/iBehance.vue";
+import iGithub from "@/components/icons/iGithub.vue";
 </script>
